d4a951d6335c60acaf60ff9416e0ffc7f4026f47
Willis <willisxwu@gmail.com>
2024-09-13 22:35:21 -0400

    feat: add plugin VitePWA in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
 import presetUno from 'unocss/preset-uno'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    UnpluginTypia(),
-    UnoCSS({
-      presets: [presetUno()],
-    }),
     react(),
+    UnpluginTypia(),
+    UnoCSS({ presets: [presetUno()] }),
+    VitePWA({ registerType: 'autoUpdate' }),
   ],
 })
